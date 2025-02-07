505398c58f58890474da04aa081b63b9d0e533fb
amitcjc <amitcjcpune@gmail.com>
2025-02-07 08:45:52 +0530

    by Amit

--- DemoGit29/src/com/cjc/Demo.java
+++ DemoGit29/src/com/cjc/Demo.java
@@ -7,6 +7,13 @@ public void m1()
 	System.out.println("This is M1--Method");
 }
 
+
+public void m2()
+{
+	
+	System.out.println("This is M2===Method");
+}
+
 public static void main(String[] args) 
 {
 System.out.println("This is main Method");	
