2063736b16ea0dad8aed7ede4b59bc1a3e06e860
amitcjc <amitcjcpune@gmail.com>
2025-02-07 08:41:31 +0530

    by CJC Team

---
+++ DemoGit29/src/com/cjc/Demo.java
@@ -0,0 +1,14 @@
+package com.cjc;
+
+public class Demo 
+{
+public void m1()
+{
+	System.out.println("This is M1--Method");
+}
+
+public static void main(String[] args) 
+{
+System.out.println("This is main Method");	
+}
+}
